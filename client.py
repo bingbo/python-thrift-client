3af92b260c5dc3f89b625730fad4a7b8cf06e39f
bingbo <ibingbo.zh@gmail.com>
2016-10-10 14:50:31 +0800

    up

--- client.py
+++ client.py
@@ -9,7 +9,8 @@ from thrift.protocol import TBinaryProtocol
 from hbase import Hbase
 from hbase.ttypes import *
 
-transport = TSocket.TSocket('140.205.176.165', 9090);
+#transport = TSocket.TSocket('140.205.176.165', 9090);
+transport = TSocket.TSocket('127.0.0.1', 9090);
 transport = TTransport.TBufferedTransport(transport)
 protocol = TBinaryProtocol.TBinaryProtocol(transport);
 
@@ -17,7 +18,14 @@ client = Hbase.Client(protocol)
 transport.open()
 
 tables = client.getTableNames()
-print tables
+for table in tables:
+    print 'found:%s' % table
+    if client.isTableEnabled(table):
+        print ' enabled table:%s' % table
+    else:
+        print ' disabled table:%s' % table
 
-client.checkAndPut('spacc_merge','rowkey:xxxxx','columns:usid:log','value:data')
+#print tables
+
+#client.checkAndPut('spacc_merge','rowkey:xxxxx','columns:usid:log','value:data')
 
